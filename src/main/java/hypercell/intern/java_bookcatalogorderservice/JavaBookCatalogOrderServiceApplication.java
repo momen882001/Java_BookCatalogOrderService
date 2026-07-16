db81c9347786e04278d086368ab655a68ba2085c
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2026-07-16 15:14:39 +0300

    Initial Spring Boot project

---
+++ src/main/java/hypercell/intern/java_bookcatalogorderservice/JavaBookCatalogOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package hypercell.intern.java_bookcatalogorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookCatalogOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBookCatalogOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hypercell/intern/java_bookcatalogorderservice/JavaBookCatalogOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hypercell.intern.java_bookcatalogorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookCatalogOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
